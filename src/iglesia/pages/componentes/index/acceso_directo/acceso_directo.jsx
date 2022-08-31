25560ed6dc8643e04f8e3ad8e210e1a12d210623
cristobal_D <cristobaldonosonuevo@gmail.com>
2022-08-30 21:27:14 -0400

    Socials

--- src/iglesia/pages/componentes/index/acceso_directo/acceso_directo.jsx
+++ src/iglesia/pages/componentes/index/acceso_directo/acceso_directo.jsx
@@ -1,9 +1,18 @@
-import { Box, VStack, Image, Link, HStack, Button} from '@chakra-ui/react'
+import { Box, VStack, Image, Link, HStack, Button, Td, Tbody,Table} from '@chakra-ui/react'
 import Styles from './acceso_directo.module.scss'
 import unused from '../../../../../assets/images/unused_black.png'
-import {FaFacebook, FaTwitter} from "react-icons/fa"
-
+import {FaFacebook, FaYoutube} from "react-icons/fa"
+import { useState } from "react";
+import { ExternalLinkIcon } from '@chakra-ui/icons'
 export default function AccesoDirecto(){
+    const [displayRedirecButtonFA, setDisplaySelect] = useState(false)
+    const handleButtonOneOnPress = () => {
+        setDisplaySelect(displayRedirecButtonFA)
+    }
+    const [displayRedirecButtonYOU, setDisplaySelectTwo] = useState(false)
+    const handleButtoTwoOnPress = () => {
+        setDisplaySelect(displayRedirecButtonYOU)
+    }
     return(
         <Box>
             <Box className={Styles.texto}>
@@ -26,14 +35,35 @@ export default function AccesoDirecto(){
                             <Image src={unused} w="2vw" margin=".4vw 0 0 .89vw" />
                         </Box >
                         <Box className={Styles.boton_texto}>
-                        <HStack>
-                            <Button colorScheme='facebook' leftIcon={<FaFacebook />}>
-                                Facebook
-                            </Button>
-                            <Button colorScheme='twitter' leftIcon={<FaTwitter />}>
-                                Twitter
-                            </Button>
+                            <Table className={Styles.UVregular}>
+                            <HStack>
+                                <Tbody>
+                                    <Td>
+                                        <Box>
+
+                                        <Button colorScheme='facebook' leftIcon={<FaFacebook />} onClick={handleButtonOneOnPress}>
+                                            {!displayRedirecButtonFA ? (
+                                                <Link href='https://web.facebook.com/Parroquia-Santo-Toribio-de-Las-Condes-474555292658302/' isExternal>
+                                                    <ExternalLinkIcon w='2vw' />
+                                                </Link>
+                                            ) : <></>}
+                                        </Button>
+                                        </Box>
+                                    </Td>
+                                        <Td>
+                                            <Box>
+                                                <Button colorScheme='red' leftIcon={<FaYoutube />}>
+                                                    {!displayRedirecButtonYOU ? (
+                                                        <Link href='https://www.youtube.com/channel/UCro3erV_F9i_WBwLpKvV4EA' isExternal>
+                                                            <ExternalLinkIcon w='2vw' />
+                                                        </Link>
+                                                    ) : <></>}
+                                                </Button>
+                                            </Box>
+                                        </Td>
+                                    </Tbody>
                             </HStack>
+                            </Table>
                         </Box>
                     </Box>
                 </Link>
