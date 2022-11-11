cbacaee4ba41fc80f2ec969c418bc17d37210069
TheCreeperZenior <thecreeperzenior@gmail.com>
2022-11-11 11:34:00 -0300

    removed mui

--- src/IglesiaApp.jsx
+++ src/IglesiaApp.jsx
@@ -1,14 +1,12 @@
 import React from 'react'
 import { AppRouter } from './router/AppRouter'
-import { AppTheme } from './theme'
+
 import { ChakraProvider } from '@chakra-ui/react'
 
 export const IglesiaApp = () => {
   return (
-    <AppTheme>
       <ChakraProvider>
         <AppRouter />
       </ChakraProvider>
-    </AppTheme>
   )
 }
--- src/auth/routes/AuthRoutes.jsx
+++ src/auth/routes/AuthRoutes.jsx
@@ -1,12 +1,11 @@
 import React from 'react'
 import {Routes, Route, Navigate} from 'react-router-dom'
-import { LoginPage, RegisterPage } from '../pages'
+import { LoginPage } from '../pages'
 
 export const AuthRoutes = () => {
   return (
     <Routes>
         <Route path="login" element={<LoginPage />} />
-        <Route path="register" element={<RegisterPage />} />
     </Routes>
   )
 }
